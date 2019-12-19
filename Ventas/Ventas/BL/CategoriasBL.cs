7bc22d4fa116e4a548244fbb64c6cd401a5d0eb0
Usuario <Usuario@QBEX>
2019-12-19 07:23:49 -0600

    Diseñando el formulario de mantenimento de productos

---
+++ Ventas/Ventas/BL/CategoriasBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas.Modelos;
+
+namespace Ventas.BL
+{
+    public class CategoriasBL
+    {
+        public List<Categoria> ListadeCategorias { get; set; }
+
+        public CategoriasBL()
+        {
+            ListadeCategorias = new List<Categoria>();
+            CrearDatosdePrueba();
+        }
+
+        private void CrearDatosdePrueba()
+        {
+            var categoria1 = new Categoria(1, "Laptops");
+            var categoria2 = new Categoria(2, "Accesorios");
+
+            ListadeCategorias.Add(categoria1);
+            ListadeCategorias.Add(categoria2);
+        }
+    }
+}
--- Ventas/Ventas/Modelos/Categoria.cs
+++ Ventas/Ventas/Modelos/Categoria.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ventas
+namespace Ventas.Modelos
 {
-    public class Producto
+    public class Categoria
     {
         public int Id { get; set; }
         public string Descripcion { get; set; }
-        public double Precio { get; set; }
-        public Categoria Categoria { get; set; }
+
+        public Categoria(int id, string desripcion)
+        {
+            Id = id;
+            Descripcion = desripcion;
+        }
     }
 }
